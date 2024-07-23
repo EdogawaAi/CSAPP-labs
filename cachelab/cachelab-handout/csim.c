8b322fe17e5594864e03f4ac349a6f6348d803c6
hoshino_cola <2835327895@qq.com>
2024-07-23 17:31:21 +0800

    finished cachelab,after referencing many blogs

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -1,85 +1,84 @@
 #include "cachelab.h"
+#include <unistd.h>
 #include <getopt.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 
-typedef struct line
+
+typedef struct
 {
-    /* data */
-    int valid;
-    int tag;
-    int stamp;
-}line;
+    int valid;//每个缓存行中v
+    int tag;//定义每一个line的tag
+    int time_tamp;//定义了时间戳,寻找每一个缓存行来的时间
+}cache_line;
 
-typedef struct cache{
-    int S;
-    int E;
-    int B;
-    line **set;
+typedef struct
+{
+    int S;//每个缓存行有s位时，S=2^s个
+    int E;//每个set中的缓存行数
+    int B;//每个缓存行有b位时,B=2^b
+    cache_line** line;
 }cache;
 
-cache* CACHE = NULL;
+
+cache* Cache = NULL;
 int miss_count = 0;
 int hit_count = 0;
 int eviction_count = 0;
 int verbose = 0;//是否打印详细信息
 char t[100];
 
-void init_cache(int s, int E, int b){
-    CACHE = (cache*) malloc(sizeof(cache));
-    CACHE ->S = 1 << s;
-    CACHE ->B = 1 << b;
-    CACHE ->E = E;
-    CACHE ->set = (line**) malloc(sizeof(line*) * (1 << s));
-    for(int i = 0; i < (1 << s); i++){
-        CACHE ->set[i] = (line*)malloc(sizeof(line) * E);
-        for(int j = 0; j <E; j++){
-            CACHE ->set[i][j].valid = 0;
-            CACHE ->set[i][j].tag = -1;
-            CACHE ->set[i][j].stamp = 0;
+void initCache(int s, int E, int b){
+    Cache = (cache*) malloc(sizeof(cache));
+    Cache->S = 1<<s;
+    Cache->E = E;
+    Cache->B = 1<<b;
+    Cache->line = (cache_line**) malloc(sizeof(cache_line*) * (1 << s));
+    for(int i = 0; i < (1<<s); i++){
+        Cache->line[i] = (cache_line*) malloc(sizeof(cache_line) * E);
+        for(int j = 0; j < E; j++){
+            Cache->line[i][j].valid = 0;
+            Cache->line[i][j].tag = -1;
+            Cache->line[i][j].time_tamp = 0;
         }
     }
-
 }
 
-//**实现LRU时间戳：最近最少使用策略
 int getIndex(int op_set, int op_tag){
-    for(int i = 0; i < CACHE ->E; i++ ){
-        if(CACHE ->set[op_set][i].valid && CACHE ->set[op_set][i].tag == op_tag){
+    for(int i = 0; i < Cache->E; i++){
+        if(Cache->line[op_set][i].valid && Cache->line[op_set][i].tag == op_tag){
             return i;
         }
     }
-    return -1;
-}//找到指定op_set中,tag
+    return -1;//有两种情况：冷不命中,或者所有的缓存行都满了
+}
 
 int isFull(int op_set){
-    for(int i = 0; i < CACHE->E; i++){
-        if(CACHE ->set[op_set][i].valid == 0){
+    for(int i = 0; i < Cache->E; i++){
+        if(Cache->line[op_set][i].valid == 0)
             return i;
-        }
     }
     return -1;
 }
 
-void update(int i, int op_s, int op_tag){
-    CACHE ->set[op_s][i].valid = 1;
-    CACHE ->set[op_s][i].tag = op_tag;
-    for(int k = 0; k < CACHE ->E; k++){
-        if(CACHE ->set[op_s][k].valid == 1){
-            CACHE ->set[op_s][k].stamp++;
-        }
+void update(int i, int op_set, int op_tag){
+    Cache->line[op_set][i].valid = 1;
+    Cache->line[op_set][i].tag = op_tag;
+    for(int k = 0; k < Cache->E; k++){
+        Cache->line[op_set][k].time_tamp++;
     }
-    CACHE ->set[op_s][i].stamp = 0;
-}
+    Cache->line[op_set][i].time_tamp = 0;
+}//每次更新缓存块，其他的时间戳++，被挤兑的缓存块置零。
 
-int find_LRU(int op_s){
+int findLRU(int op_set){
     int maxIndex = 0;
     int maxStamp = 0;
-    for(int i = 0; i < CACHE ->E; i++){
-        if(CACHE ->set[op_s][i].stamp > maxStamp){
-            maxStamp = CACHE ->set[op_s][i].stamp;
+    for(int i = 0; i < Cache->E; i++){
+        if(Cache->line[op_set][i].time_tamp > maxStamp){
             maxIndex = i;
+            maxStamp = Cache->line[op_set][i].time_tamp;
         }
     }
     return maxIndex;
@@ -87,7 +86,7 @@ int find_LRU(int op_s){
 
 void updateInfo(int op_set, int op_tag){
     int index = getIndex(op_set, op_tag);
-    //未命中
+    //是否命中
     if(index == -1){
         miss_count++;
         if(verbose){
@@ -99,10 +98,10 @@ void updateInfo(int op_set, int op_tag){
             if(verbose){
                 printf("eviction\n");
             }
-            i = find_LRU(op_set);
+            i = findLRU(op_set);
         }
         update(i, op_set, op_tag);
-    }else{
+    }else {
         hit_count++;
         if(verbose){
             printf("hit\n");
@@ -110,58 +109,58 @@ void updateInfo(int op_set, int op_tag){
         update(index, op_set, op_tag);
     }
 }
-//
+
 void freeCache(){
-    for(int i = 0; i < CACHE->S; i++){
-        free(CACHE->set[i]);
+    for(int i = 0; i < Cache->E; i++){
+        free(Cache->line[i]);
     }
-    free(CACHE->set);
-    free(CACHE);
-    CACHE = NULL;
+    free(Cache->line);
+    free(Cache);
 }
 
 void getTrace(int s, int E, int b){
-    FILE* pFile;
-    pFile = fopen(t, "r");
-    if(pFile == NULL){
+    FILE* fp;
+    fp = fopen(t, "r");
+    if(fp == NULL){
         exit(-1);
     }
     char identifier;
     unsigned address;
     int size;
-    while(fscanf(pFile, "%c %x %d", &identifier, &address, &size) > 0){
+    //读取M 20,1/L 19,3
+    while(fscanf(fp, " %c %x %d", &identifier, &address, &size) > 0){
         int op_tag = address >> (s + b);
-        int op_s = (address >> b) & ((unsigned)(-1) >> (8 * sizeof(unsigned) - s));
+        int op_set = (address >> b) & ((1 << s) - 1);
         switch (identifier)
         {
         case 'M':
-            updateInfo(op_s, op_tag);
-            updateInfo(op_s, op_tag);
+            updateInfo(op_set, op_tag);
+            updateInfo(op_set, op_tag);
             break;
         case 'L':
-            updateInfo(op_s, op_tag);
+            updateInfo(op_set, op_tag);
             break;
-        case 's':
-            updateInfo(op_s, op_tag);
+        case 'S':
+            updateInfo(op_set, op_tag);
             break;
         }
     }
-    fclose(pFile);
-
+    fclose(fp);
 }
+
 
 int main(int argc, char* argv[])
 {
     //printSummary(0, 0, 0);
     char opt;
     int s, E, b;
-    while(-1 != (opt = getopt(argc, argv, "hvs:E:b:t:")))
+    while (-1 != (opt = getopt(argc, argv, "hvs:E:b:t:")))
     {
         switch (opt)
         {
         case 'h':
             printf("input format wrong");
-            exit(0);
+            break;
         case 'v':
             verbose = 1;
             break;
@@ -178,11 +177,11 @@ int main(int argc, char* argv[])
             strcpy(t, optarg);
             break;
         default:
-            printf("input format wrong!");
-            exit(-1);
+            printf("input format wrong");
+            break;
         }
     }
-    init_cache(s, E, b);
+    initCache(s, E, b);
     getTrace(s, E, b);
     freeCache();
     printSummary(hit_count, miss_count, eviction_count);
--- cachelab/cachelab-handout/trans.c
+++ cachelab/cachelab-handout/trans.c
@@ -22,6 +22,123 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if(M == 32){
+        for(int i = 0; i < N; i+=8){
+            for(int j = 0; j < M; j+=8){
+                for(int k = 0; k < 8; k++){
+                    int a = A[i + k][j];
+                    int b = A[i + k][j + 1];
+                    int c = A[i + k][j + 2];
+                    int d = A[i + k][j + 3];
+                    int e = A[i + k][j + 4];
+                    int f = A[i + k][j + 5];
+                    int g = A[i + k][j + 6];
+                    int h = A[i + k][j + 7];
+                    B[j][i + k] = a;
+                    B[j + 1][i + k] = b;
+                    B[j + 2][i + k] = c;
+                    B[j + 3][i + k] = d;
+                    B[j + 4][i + k] = e;
+                    B[j + 5][i + k] = f;
+                    B[j + 6][i + k] = g;
+                    B[j + 7][i + k] = h;
+                }
+            }
+        }
+    }
+    if(M == 64){
+{
+    int i, j;
+
+    for (i = 0; i < N; i += 8) {      // 枚举每八行
+        for (j = 0; j < M; j += 8) {  // 枚举每八列
+            // 这里用这些临时变量，如果你查看过A和B的地址的话，你会发现A和B的地址差距是64的整数倍（0x40000），
+            // 那么 直接赋值的话，在对角线的时候 每一个Load A[i][i]紧跟Store B[i][i],将造成比较多的
+            // eviction
+            int temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, cnt;
+            // 1块8*8，我们分成4块来做，每一块4*4
+            // 这是 左上，且将左下的块移动到 B中的右上，这样是为了更高效地利用cache
+            for (cnt = 0; cnt < 4; ++cnt, ++i) {  // 枚举0~8中的每一行，一行八列
+                temp1 = A[i][j];  // 这样我们就一次取出了8个元素，我们的A的miss就只有一次了 原始4*4
+                                  // 则是两次
+                temp2 = A[i][j + 1];
+                temp3 = A[i][j + 2];
+                temp4 = A[i][j + 3];  // 左上
+
+                temp5 = A[i][j + 4];  // 右上
+                temp6 = A[i][j + 5];
+                temp7 = A[i][j + 6];
+                temp8 = A[i][j + 7];
+
+                B[j][i] = temp1;  // 左上翻转
+                B[j + 1][i] = temp2;
+                B[j + 2][i] = temp3;
+                B[j + 3][i] = temp4;
+
+                B[j][i + 4] = temp5;  //将A中右上 存到B中的右上，这也是全部命中的
+                B[j + 1][i + 4] = temp6;
+                B[j + 2][i + 4] = temp7;
+                B[j + 3][i + 4] = temp8;
+            }
+            i -= 4;
+            // 处理A中左下
+            for (cnt = 0; cnt < 4; ++j, ++cnt) {  // 枚举0~8中的每一行，一行八列
+                temp1 = A[i + 4][j];
+                temp2 = A[i + 5][j];
+                temp3 = A[i + 6][j];
+                temp4 = A[i + 7][j];  // 拿到左下的元素
+
+                // 因为我们这里本来就要处理 右上，所以这不会带来更多的miss
+                temp5 = B[j][i + 4];  // 拿到我们之前赋给B右上的，也就是A右上的元素
+                temp6 = B[j][i + 5];
+                temp7 = B[j][i + 6];
+                temp8 = B[j][i + 7];
+
+                B[j][i + 4] = temp1;  //将左下翻转到B右上
+                B[j][i + 5] = temp2;
+                B[j][i + 6] = temp3;
+                B[j][i + 7] = temp4;
+
+                // 这一步，也不会带来更多的miss因为 i + 4 <= j + 4 <= i + 7 恒成立
+                // 所以每次带来的 eviction 和 store B[j][i +
+                // 4]只会带来一次MISS，和原来的操作是一样的
+
+                B[j + 4][i] = temp5;  //将原B右上 赋值到 B左下， 这样A右上也就完成了翻转
+                B[j + 4][i + 1] = temp6;
+                B[j + 4][i + 2] = temp7;
+                B[j + 4][i + 3] = temp8;
+            }
+            j -= 4;
+            j += 4;  // 处理第四块 右下
+            for (i += 4, cnt = 0; cnt < 4; ++cnt, ++i) {
+                temp1 = A[i][j];  // 第四块没有任何改动， 和原来效果是一样的
+                temp2 = A[i][j + 1];
+                temp3 = A[i][j + 2];
+                temp4 = A[i][j + 3];
+
+                B[j][i] = temp1;
+                B[j + 1][i] = temp2;
+                B[j + 2][i] = temp3;
+                B[j + 3][i] = temp4;
+            }
+            i -= 8, j -= 4;
+        }
+    }
+}
+    }
+    if(M == 61){
+        {
+    int i, j;
+    int ex = 17;  // 17是我测出来最小的了 不过像 16,18，17，21这些也都是满分的
+    for (i = 0; i < N; i += ex) {      // 枚举每ex行
+        for (j = 0; j < M; j += ex) {  // 枚举每ex列
+                                       // 下面开始转置这个ex * ex 的块
+            for (int x = i; x < N && x < i + ex; ++x)
+                for (int y = j; y < M && y < j + ex; ++y) B[y][x] = A[x][y];
+        }
+    }
+}
+    }
 }
 
 /* 
